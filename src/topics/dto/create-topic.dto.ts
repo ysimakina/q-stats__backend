10a069e72a180d08d4d1fa0ae62f12cf24918e73
vkarelina <v.karelina@tg.dunice.net>
2024-10-22 09:33:16 +0300

    fix: topic dto

--- src/topics/dto/create-topic.dto.ts
+++ src/topics/dto/create-topic.dto.ts
@@ -1,1 +1,6 @@
-export class CreateTopicDto {}
+import { IsString } from 'class-validator';
+
+export class CreateTopicDto {
+  @IsString({ message: 'Name must be a string' })
+  readonly name: string;
+}
--- src/topics/topics.service.ts
+++ src/topics/topics.service.ts
@@ -1,17 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import { plainToInstance } from 'class-transformer';
 
 import { Topic } from './entities/topic.entity';
+import { OutputGetTopicDto } from './dto/output-get-topic.dto';
 
 @Injectable()
 export class TopicsService {
   constructor(@InjectModel(Topic) private userRepository: typeof Topic) {}
 
-  findAll() {
-    return this.userRepository.findAll();
+  async findAll(): Promise<OutputGetTopicDto[]> {
+    const topics = await this.userRepository.findAll({
+      attributes: ['id', 'name'],
+    });
+
+    return plainToInstance(OutputGetTopicDto, topics, {
+      excludeExtraneousValues: true,
+    });
   }
 
-  findOne(id: number) {
-    return this.userRepository.findByPk(id);
+  async findOne(id: number): Promise<OutputGetTopicDto> {
+    const topic = await this.userRepository.findByPk(id, {
+      attributes: ['id', 'name'],
+    });
+
+    return plainToInstance(OutputGetTopicDto, topic, {
+      excludeExtraneousValues: true,
+    });
   }
 }
