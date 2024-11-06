b5ad816fe5cec2180f2822b93c6f0ae50f8b2ca6
vkarelina <v.karelina@tg.dunice.net>
2024-11-06 09:55:09 +0300

    refactor: delete console.log

--- src/user-questions/user-questions.controller.ts
+++ src/user-questions/user-questions.controller.ts
@@ -42,7 +42,6 @@ export class UserQuestionsController {
       createUserQuestionDto, userId
     );
 
-    console.log(1111)
     return plainToInstance(OutputUserQuestionDto, question, {
       excludeExtraneousValues: true,
     });
